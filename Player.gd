489c11e08d90da63fa7e72f4fe7c25ad9d607f6f
🐿️ <1071472+clanam@users.noreply.github.com>
2020-06-10 06:28:41 -0700

    [🖋️] add tutorial's player position and velocity update code

--- Player.gd
+++ Player.gd
@@ -12,4 +12,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = _update_and_return_sprite_velocity()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+
+func _update_and_return_sprite_velocity():
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	return velocity
+	
