57f7aab169d22e8bca6d75357b74137515e3150e
🐿️ <1071472+clanam@users.noreply.github.com>
2020-06-13 08:03:23 -0700

    [⏰] add timer scripts in main scene

--- Main.gd
+++ Main.gd
@@ -20,3 +20,30 @@ func new_game():
 func _ready():
 	# Make sure a new random seed is used each time.
 	randomize()
+
+
+func _on_MobTimer_timeout():
+	# Choose a random location on Path2D.
+	$MobPath/MobSpawnLocation.offset = randi()
+	# Create a Mob instance and add it to the scene.
+	var mob = Mob.instance()
+	add_child(mob)
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+	# Set the mob's position to a random location.
+	mob.position = $MobPath/MobSpawnLocation.position
+	# Add some randomness to the direction.
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	# Set the velocity (speed & direction).
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
