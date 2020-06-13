4499a4765f0eb17129cc54a3352248f7e07b012e
🐿️ <1071472+clanam@users.noreply.github.com>
2020-06-13 07:53:31 -0700

    [🦠] connect game_over function to player hit signal

--- Main.gd
+++ Main.gd
@@ -5,6 +5,17 @@ export (PackedScene) var Mob
 var score
 
 
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Make sure a new random seed is used each time.
