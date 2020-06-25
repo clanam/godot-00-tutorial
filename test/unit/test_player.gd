7a9cab4a178807098af96262669e05bf16378913
🐿️ <1071472+clanam@users.noreply.github.com>
2020-06-25 07:38:35 -0700

    [💮] get initial tests passing

--- test/unit/test_player.gd
+++ test/unit/test_player.gd
@@ -1,22 +1,22 @@
 extends "res://addons/gut/test.gd"
 
-var Player = load('res://Player.gd')
+var Player = load('res://Player.tscn')
 var _player = null
 
 func before_each():
-	_player = Player.new()
+	_player = Player.instance()
 	# _player = autofree(Player.new())
 
 
 func test_start_sets_player_position():
 	var pos = Vector2(3, 2)
 	_player.start(pos)
-	assert_true(_player.position.x, 3)
-	assert_true(_player.position.y, 2)
+	assert_eq(_player.position.x, 3)
+	assert_eq(_player.position.y, 2)
 
 
 func test_start_sets_player_target():
 	var pos = Vector2(41, 25)
 	_player.start(pos)
-	assert_true(_player.target.x, 41)
-	assert_true(_player.target.y, 25)
+	assert_eq(_player.target.x, 41)
+	assert_eq(_player.target.y, 25)
