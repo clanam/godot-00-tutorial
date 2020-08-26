4230d6f5ec8383f94b32ea4d8f1e05a5dca8f67b
🐿️ <1071472+clanam@users.noreply.github.com>
2020-08-26 08:19:40 -0700

    [🥮] add a touch event test

--- test/unit/test_player.gd
+++ test/unit/test_player.gd
@@ -10,15 +10,13 @@ func before_each():
 func test_start_sets_player_position():
 	var pos = Vector2(3, 2)
 	_player.start(pos)
-	assert_eq(_player.position.x, 3.0)
-	assert_eq(_player.position.y, 2.0)
+	assert_eq(_player.position, Vector2(3, 2), "Position should be set")
 
 
 func test_start_sets_player_target():
 	var pos = Vector2(41, 25)
 	_player.start(pos)
-	assert_eq(_player.target.x, 41.0)
-	assert_eq(_player.target.y, 25.0)
+	assert_eq(_player.target, Vector2(41, 25), "Target should be set")
 
 
 func test_start_sets_player_animation_walk():
@@ -26,5 +24,17 @@ func test_start_sets_player_animation_walk():
 	_player.start(pos)
 	var sprite = _player.get_node('AnimatedSprite')
 	assert_eq(sprite.animation, "walk")
-	assert_eq(sprite.flip_h, false)
-	assert_eq(sprite.flip_v, false)
+	assert_eq(sprite.flip_h, false, "Horizontal flip should be false")
+	assert_eq(sprite.flip_v, false, "Vertical flip should be false")
+
+
+func test_input_touch_event_sets_target():
+	_player.start(Vector2(10, 20))
+	assert_eq(_player.target, Vector2(10, 20), "Initial target should be set")
+
+	var event = InputEventScreenTouch.new()
+	event.position = Vector2(30, 25)
+	event.pressed = true
+
+	_player._input(event)
+	assert_eq(_player.target, Vector2(30, 25), "Target should be updated")
